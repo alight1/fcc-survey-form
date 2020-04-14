6afce593b69dc6c043815a4bef6a0af844a9844a
alight1 <andrelight.al@gmail.com>
2020-04-14 13:37:39 -0600

    Move freeCodeCamp testing script to index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-require('../src/style.css');
-import bodyContent from '../src/body-content.html'
+require("../src/style.css");
+import bodyContent from "../src/body-content.html";
 
 document.getElementById("body").innerHTML += bodyContent;
-
-//Add freeCodeCamp script (cannot add to dynamically loaded HTML like body-content.html)
-let fccScript = document.createElement('script');
-fccScript.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
-document.head.appendChild(fccScript);
