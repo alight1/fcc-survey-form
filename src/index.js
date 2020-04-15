3bde3702b7a5f9191794e8403c0b7e5f695d10f7
alight1 <andrelight.al@gmail.com>
2020-04-14 18:25:21 -0600

    Meet basic freeCodeCamp project requirements

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ require("../src/style.css");
 import bodyContent from "../src/body-content.html";
 
 document.getElementById("body").innerHTML += bodyContent;
+
+//Append freeCodeCamp test utility (does not work when added directly to HTML)
+var fccScript = document.createElement("script");
+fccScript.src =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+document.head.appendChild(fccScript);
